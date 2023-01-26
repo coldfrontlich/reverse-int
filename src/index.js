1a0f568b8d0fde14fd5dd986167f7b75073d430f
coldfrontlich <coldfrontlich@gmail.com>
2023-01-26 15:00:44 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) {
+    n = Math.abs(n);
+  }
+  let stringNumber = String(n);
+  let arrayNumber = stringNumber.split('');
+  let reverseArrayNumber = arrayNumber.reverse();
+  let answer = reverseArrayNumber.join('');
+  return Number(answer);
 }
